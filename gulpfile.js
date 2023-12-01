4f2ee33b8a153f45d5f5eabe698d0014e2ef9978
Breno Pereira <brenopereirabetti@gmail.com>
2023-12-01 10:19:07 -0300

    organizando o ambiente de trabalho

---
+++ gulpfile.js
@@ -0,0 +1,17 @@
+const gulp = require('gulp')
+const sass = require('gulp-sass')(require('sass'))
+//const imagemin = require('gulp-imagemin')
+
+function compilaSass() {
+    return gulp.src('./src/styles/*.scss')
+        .pipe(sass({
+            outputStyle: 'compressed'
+        }))
+        .pipe(gulp.dest('./dist/styles'))
+}
+
+exports.default = gulp.parallel(compilaSass)
+exports.watch = function() {
+    gulp.watch('./src/styles/*.scss', gulp.parallel(compilaSass))
+    
+}
