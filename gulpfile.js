dd3750dfa75c5297e9af2d898eb811715eb30a82
Breno Pereira <brenopereirabetti@gmail.com>
2023-12-01 14:22:00 -0300

    construção do header

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,6 @@
 const gulp = require('gulp')
 const sass = require('gulp-sass')(require('sass'))
-//const imagemin = require('gulp-imagemin')
+const imagemin = require('gulp-imagemin')
 
 function compilaSass() {
     return gulp.src('./src/styles/*.scss')
